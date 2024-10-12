740193239feda12e715077ea7439f1e1bda1bd4d
Ana <anaju@gmail.com>
2024-10-12 20:23:25 -0300

    Modelagem completa da tabela de avaliacao! Com Models, Controllers e Routes!

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\ReceitaController;
 use App\Http\Controllers\ComentarioController;
-
+use App\Http\Controllers\AvaliacaoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +26,10 @@ Route::post('comentario/', [ComentarioController::class, 'store']);
 Route::get('comentario/{usuario_id}/{post_id}', [ComentarioController::class, 'show']);
 Route::put('comentario/{usuario_id}/{post_id}', [ComentarioController::class, 'update']);
 Route::delete('comentario/{usuario_id}/{post_id}', [ComentarioController::class, 'destroy']);
+// Rotas das Avaliações
+Route::get('avaliacao/', [AvaliacaoController::class, 'index']);
+Route::post('avaliacao/', [AvaliacaoController::class, 'store']);
+Route::get('avaliacao/{usuario_id}/{post_id}', [AvaliacaoController::class, 'show']);
+Route::put('avaliacao/{usuario_id}/{post_id}', [AvaliacaoController::class, 'update']);
+Route::delete('avaliacao/{usuario_id}/{post_id}', [AvaliacaoController::class, 'destroy']);
+
